c067384849f17d865c095544dd23c21339efac22
dart-wakar <wakar@whitelogic.in>
2017-04-06 18:51:11 +0530

    Generic class based views

--- snippets/views.py
+++ snippets/views.py
@@ -1,50 +1,12 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.http import Http404
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-
-# Create your views here.
-
-class SnippetList(APIView):
-    def get(self, request, format=None):
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets,many=True)
-        return Response(serializer.data)
-    def post(self,request,format=None):
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+from rest_framework import generics
 
 
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
 
-class SnippetDetail(APIView):
-    def get_objects(self,pk):
-        try:
-            return Snippet.objects.get(pk=pk)
-        except Snippet.DoesNotExist:
-            return Http404
-
-    def get(self,request,pk,format=None):
-        snippet = self.get_objects(pk)
-        serializer = SnippetSerializer(snippet)
-        return Response(serializer.data)
-
-    def put(self,request,pk,format=None):
-        snippet = self.get_objects(pk)
-        serializer = SnippetSerializer(snippet,data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self,request,pk,format=None):
-        snippet = self.get_objects(pk)
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
